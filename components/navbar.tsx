8b7613a5536ea4bb8e57904b1deb705744e1a175
Gogo602 <gogomarosi61@gmail.com>
2025-04-13 20:47:30 +0300

    initial commit

---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+
+export default function Navbar() {
+    return (
+        <nav className='fixed w-full z-50 bg-black/50 back-drop-blur-2xl'>
+            <div className="max-width-7xl mx-auto px-6 py-3">
+                <div className='flex items-center justify-between'>
+                    <div className="relative h-8 w-8 rounded-full over-flow">
+                        <div className="absolute rounded-full inset-0 bg-gradient-to-r from-blue-500 to-purple-500">
+
+                        </div>
+                    </div>
+                </div>
+           </div>
+        </nav>
+        
+    );
+}
